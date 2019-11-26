fdc0cf0c34294735c79a133288c875628b149eb2
TheEasyLemon <dawsonren@gmail.com>
2019-11-26 13:41:35 -0600

    Volatility and Variance Update

--- Stock.java
+++ Stock.java
@@ -4,11 +4,10 @@ public class Stock {
   //index = days since start, value = price
   private ArrayList<Double> values = new ArrayList<>();
   private double price;
-  private int day;
+  private int day = 0;
   private String name;
   //volatility is a multiplier, where the change of price is given by:
   //change in price = volatility x (random number between 0.0 and 1.0)
-  private double volatility;
   private Random rd = new Random();
   private final int function = rd.nextInt(5);
   
@@ -20,16 +19,16 @@ public class Stock {
   //sinh(x) is about the same but values above 0.5 are slightly tuned up (> 0.5 up)
   public Stock(String n) {
     name=n;
-    //day 0 at initialization
-    day = 0;
     //price will be between $15 and $200
     price = rd.nextInt(186) + 15;
-    //volatility is scaled to the total price, so percent change per day
-    //is capped at 5%
-    volatility = price / 20;
     values.add(day, price);
   }
   public double nextDay() {
+    //volatility must be calculated before bumping day
+    double volatility = getCurrentPrice() / 10;
+    
+    day++;
+    
     //highlow controls whether it's going to up or down
     //highlow = 0 means down, highlow = 1 means up
     int highlow = rd.nextInt(2);
@@ -38,7 +37,6 @@ public class Stock {
     } else {
       price += functionize(rd.nextDouble()) * volatility;
     }
-    day++;
     values.add(day, price);
     
     //calculates change
@@ -62,14 +60,14 @@ public class Stock {
       }
       return given;
   }
-  public double getPrice() {
+  public double getFirstPrice() {
     return price;
   }
-  public double getPrice(int day) {
-    return values.get(day);
+  public double getPrice(int d) {
+    return values.get(d);
   }
-  public int getDay() {
-    return day;
+  public double getCurrentPrice() {
+    return values.get(day);
   }
   public String getName()
   {
