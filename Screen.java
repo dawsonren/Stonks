a9f17d849c506ddf26185fd0a44def4ed244f79f
TheEasyLemon <dawsonren@gmail.com>
2019-12-10 13:07:13 -0600

    Take out Buy/Sell Java

--- Screen.java
+++ Screen.java
@@ -1,7 +1,4 @@
-//TODO: Handle buy/sell logic
-
-//Creates GUI
-import javax.swing.*;
+/import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.text.DecimalFormat;
@@ -10,12 +7,12 @@ import java.util.*;
 public class Screen extends JFrame implements ActionListener
 {
     private static DecimalFormat two = new DecimalFormat ("0.00");
-    private final String spacer = "    ";
-    private final Font smallFont = new Font("Times New Roman", Font.PLAIN, 18);
-    private final Font bigFont = new Font("Times New Roman", Font.PLAIN, 20);
+    private final String SPACER = "    ";
+    private final Font SMALL_FONT = new Font("Times New Roman", Font.PLAIN, 18);
+    private final Font BIG_FONT = new Font("Times New Roman", Font.PLAIN, 20);
     
     private final int totalStocks = 5;
-    private final String[] stockNames = {"Apple", "Microsoft", "Samsung", "Nintendo", "PlayStation"};
+    public final String[] stockNames = {"Apple", "Microsoft", "Samsung", "Nintendo", "PlayStation"};
     public final double startingCash = 100000.0;
     
     //overall window container
@@ -25,8 +22,8 @@ public class Screen extends JFrame implements ActionListener
     private JButton[] stockButtons = new JButton[totalStocks];
     private JButton newDay;
     
-    //m1 is overview, m2 is buy, m3 is sell, m4 is help
-    private JMenuItem m1, m2, m3, m4;
+    //m1 is overview, m2 is buy/sell, m3 is help
+    private JMenuItem m1, m2, m3;
     
     //JPanels (p1 is the button panel, p2 is the selected stock screen, and p3 is for total earnings, time, ect.)
     JPanel p1 = new JPanel();
@@ -39,15 +36,15 @@ public class Screen extends JFrame implements ActionListener
     private int stockIndexSelected;
     
     //stocks is used for accessing stock information in general
-    private Stock[] stocks = new Stock[totalStocks];
+    public Stock[] stocks = new Stock[totalStocks];
     
     //portfolio is used for managing the portfolio
-    private Portfolio portfolio = new Portfolio(totalStocks, startingCash);
+    public Portfolio portfolio = new Portfolio(totalStocks, startingCash);
     
     //labels cash, stockvalue, time, and networth go on the bottom. Info controls what's said in p2.
     private JLabel cash, stockValue, netWorth, time, graph, info;
     
-    private int day = 0;
+    public int day = 0;
     
     public Screen()
     {
@@ -73,44 +70,40 @@ public class Screen extends JFrame implements ActionListener
         p3.setLayout(new GridLayout(1,5));
         
         //Bottom of screen (cash, stockValue, netWorth, day and NewDay button)
-        cash = new JLabel("Cash: "+ two.format(portfolio.getCash()));
+        cash = new JLabel(SPACER + "Cash: "+ two.format(portfolio.getCash()));
         stockValue = new JLabel("Stock Value: "+ two.format(portfolio.getValue()));
         netWorth = new JLabel("Net Worth: "+ two.format(portfolio.getNetWorth()));
-        time= new JLabel("                       " + day +" days");
+        time= new JLabel(SPACER + SPACER + SPACER + day +" days");
         newDay=new JButton("New Day");
         
         //menu for what screen the player is looking at (overview, buy, sell, help)
         JMenuBar bar = new JMenuBar();
         JMenu menu= new JMenu("Menu");
         m1 = new JMenuItem("Overview");
-        m1.setFont(smallFont);
+        m1.setFont(SMALL_FONT);
         m1.addActionListener(this);
-        m2 = new JMenuItem("Buy");
-        m2.setFont(smallFont);
+        m2 = new JMenuItem("Buy and Sell");
+        m2.setFont(SMALL_FONT);
         m2.addActionListener(this);
-        m3 = new JMenuItem("Sell");
-        m3.setFont(smallFont);
+        m3 = new JMenuItem("Info");
+        m3.setFont(SMALL_FONT);
         m3.addActionListener(this);
-        m4 = new JMenuItem("Help");
-        m4.setFont(smallFont);
-        m4.addActionListener(this);
-        
+
         //JMenu for swtching what you are seeing
         menu.add(m1);
         menu.add(m2);
         menu.add(m3);
-        menu.add(m4);
         bar.add(menu);
         setJMenuBar(bar);
-        menu.setFont(bigFont);
+        menu.setFont(BIG_FONT);
         
         
         //set Fonts for bottom of screen
-        cash.setFont(bigFont);
-        stockValue.setFont(bigFont);
-        netWorth.setFont(bigFont);
-        time.setFont(bigFont);
-        newDay.setFont(bigFont);
+        cash.setFont(BIG_FONT);
+        stockValue.setFont(BIG_FONT);
+        netWorth.setFont(BIG_FONT);
+        time.setFont(BIG_FONT);
+        newDay.setFont(BIG_FONT);
         newDay.addActionListener(this);
         
         //create bottom bar
@@ -126,8 +119,7 @@ public class Screen extends JFrame implements ActionListener
         win.add(p3, BorderLayout.SOUTH);
 
         setVisible(true);
-        setSize(1000,650);
-
+        setSize(1440,800);
     }
 
     public void actionPerformed (ActionEvent e)
@@ -135,87 +127,8 @@ public class Screen extends JFrame implements ActionListener
         if (e.getSource() == m1) {
             
         } else if(e.getSource() == m2) {
-            JTextField numShares = new JTextField(5);
-            JComboBox chooseStock = new JComboBox(stockNames);
-            JLabel errorMessage = new JLabel();
-            
-            boolean again = false;
-            
-            JPanel myPanel = new JPanel();
-            myPanel.add(new JLabel("Buy "));
-            myPanel.add(numShares);
-            myPanel.add(new JLabel(" shares of "));
-            myPanel.add(chooseStock);
-            myPanel.add(errorMessage);
-            
-            do {
-                int result = JOptionPane.showConfirmDialog(null, myPanel, "Buy", JOptionPane.OK_CANCEL_OPTION);
-                
-                int num = Integer.parseInt(numShares.getText());
-                
-                if (num <= 0) {
-                    errorMessage.setText("Enter a number of shares above 0.");
-                    again = true;
-                } else {
-                    again = false;
-                }
-                
-                if (result == JOptionPane.OK_OPTION) {
-                    int confirm = JOptionPane.showConfirmDialog(null, "Confirm", "Are you sure?", JOptionPane.YES_NO_OPTION);
-                    if (confirm == JOptionPane.YES_OPTION) {
-                        //which stock selected, how many?
-                        //files the information into the portfolio
-                        int stockIndex = chooseStock.getSelectedIndex();
-                        Stock selected = stocks[stockIndex];
-                        
-                        double cost = selected.getPrice(day) * num;
-                            
-                        //adds stock to portfolio
-                        portfolio.addStock(selected, num);
-                    }
-                }
-            } while (again);
-            //updates the statistics on the bottom
-            updateBottomBar();
+            BuySellFrame frame = new BuySellFrame();
         } else if (e.getSource() == m3) {
-            JTextField numShares = new JTextField(5);
-            //only gets the stocks available in the portfolio
-            JComboBox chooseStock = new JComboBox(portfolio.getStockNames());
-            
-            JPanel myPanel = new JPanel();
-            myPanel.add(new JLabel("Sell "));
-            myPanel.add(numShares);
-            myPanel.add(new JLabel(" shares of "));
-            myPanel.add(chooseStock);
-            
-            boolean again = false;
-            
-            //do it again if you are trying to sell more than you have
-            do {
-                int result = JOptionPane.showConfirmDialog(null, myPanel, "Sell", JOptionPane.OK_CANCEL_OPTION);
-                
-                if (result == JOptionPane.OK_OPTION) {
-                    int confirm = JOptionPane.showConfirmDialog(null, "Confirm", "Are you sure?", JOptionPane.YES_NO_OPTION);
-                    if (confirm == JOptionPane.YES_OPTION) {
-                        //which stock selected, how many?
-                        //files the information into the portfolio
-                        int stockIndex = chooseStock.getSelectedIndex();
-                        Stock selected = stocks[stockIndex];
-                        int num = Integer.parseInt(numShares.getText());
-                        
-                        System.out.println("Index: " + stockIndex);
-                        System.out.println("NumL: " + num);
-                        
-                        double cost = selected.getPrice(day) * num;
-                        
-                        //stockIndex is the same as the stocks, i.e. 0 = Apple, 1 = etc...
-                        again = portfolio.removeStock(selected, num);
-                    }
-                }
-            } while (again);
-            //updates the statistics on the bottom
-            updateBottomBar();
-        } else if (e.getSource() == m4) {
             
         }
         
@@ -233,7 +146,6 @@ public class Screen extends JFrame implements ActionListener
         
         //next day button
         if (e.getSource() == newDay) {
-            System.out.println("Day Forward");
             day++;
             //update buttons must come first because it updates stock information
             updateButtons();
@@ -247,7 +159,7 @@ public class Screen extends JFrame implements ActionListener
             infoScreen = temp;
         }
     }
-
+    
     public void getStockList()
     {
         for (int x = 0; x < totalStocks; x++) {
@@ -255,8 +167,8 @@ public class Screen extends JFrame implements ActionListener
             stocks[x] = new Stock(stockNames[x]);
             
             //sets stock buttons
-            stockButtons[x] = new JButton(spacer + stocks[x].getName() + spacer + "0.00%");
-            stockButtons[x].setFont(smallFont);
+            stockButtons[x] = new JButton(SPACER + stocks[x].getName() + SPACER + "0.00%");
+            stockButtons[x].setFont(SMALL_FONT);
             stockButtons[x].addActionListener(this);
             
             //adds stock buttons to p1
@@ -277,26 +189,26 @@ public class Screen extends JFrame implements ActionListener
             double change = stocks[x].nextDay();
             if (change >= 0) {
                 stockButtons[x].setForeground(new Color(0, 153, 0));
-                stockButtons[x].setText(spacer + stocks[x].getName() + spacer + "+" + two.format(change) + "%");
+                stockButtons[x].setText(SPACER + stocks[x].getName() + SPACER + "+" + two.format(change) + "%");
             } else {
                 stockButtons[x].setForeground(Color.RED);
-                stockButtons[x].setText(spacer + stocks[x].getName() + spacer + two.format(change) + "%");
+                stockButtons[x].setText(SPACER + stocks[x].getName() + SPACER + two.format(change) + "%");
             }
         }
     }
     
     public void updateBottomBar() {
-        cash.setText("Cash: "+ two.format(portfolio.getCash()));
+        cash.setText(SPACER + "Cash: "+ two.format(portfolio.getCash()));
         stockValue.setText("Stock Value: "+ two.format(portfolio.getValue()));
         netWorth.setText("Net Worth: "+ two.format(portfolio.getNetWorth()));
-        time.setText("                       " + day +" days");
+        time.setText(SPACER + SPACER + SPACER + day +" days");
     }
     
     public JPanel homeScreen() {
         JPanel home = new JPanel();
         
         JLabel title = new JLabel("Your Portfolio:");
-        title.setFont(bigFont);
+        title.setFont(BIG_FONT);
         home.add(title);
         
         JPanel stockScreen = new JPanel();
@@ -304,11 +216,11 @@ public class Screen extends JFrame implements ActionListener
         //Left column is stock name, right column is number of stocks owned
         stockScreen.setLayout(new GridLayout(portfolio.stocksOwned(), 2));
         for (Stock stock : portfolio.getStocks()) {
-            JLabel name = new JLabel(stock.getName() + spacer);
-            name.setFont(smallFont);
+            JLabel name = new JLabel(stock.getName() + SPACER);
+            name.setFont(SMALL_FONT);
             stockScreen.add(name);
             JLabel num = new JLabel(Integer.toString(portfolio.getShares(stock)));
-            num.setFont(smallFont);
+            num.setFont(SMALL_FONT);
             stockScreen.add(num);
         }
         
