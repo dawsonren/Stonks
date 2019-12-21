3b2f1bf88059da3b4a746e65859ca07d4b132676
TheEasyLemon <dawsonren@gmail.com>
2019-12-21 09:50:28 -0600

    Add final controlling variables to Stock_Simulator

--- Stock_Simulator_2000.java
+++ Stock_Simulator_2000.java
@@ -1,6 +1,11 @@
 public class Stock_Simulator_2000
 {
     public static Screen go;
+    
+    public static final String[] stockNames = {"Apple", "Microsoft", "Samsung", "Nintendo", "PlayStation"};
+    public static final int totalStocks = 5;
+    public static final double startingCash = 100000.0;
+    
     public static void main(String[] args) {
         go = new Screen();
     }
