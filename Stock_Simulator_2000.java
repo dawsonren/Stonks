31677be0e782a15fb1529ebce1425bcb18bad575
TheEasyLemon <dawsonren@gmail.com>
2020-01-08 13:51:53 -0600

    Update Stock_Simulator_2000.java

--- Stock_Simulator_2000.java
+++ Stock_Simulator_2000.java
@@ -2,8 +2,8 @@ public class Stock_Simulator_2000
 {
     public static Screen go;
     
-    public static final String[] stockNames = {"Apple", "Microsoft", "Samsung", "Nintendo", "PlayStation"};
-    public static final int totalStocks = 5;
+    public static final String[] stockNames = {"Apple", "Microsoft", "Samsung", "Nintendo", "PlayStation", "Amazon"};
+    public static final int totalStocks = stockNames.length;
     public static final double startingCash = 100000.0;
     
     public static void main(String[] args) {
